803038c8e6be5e8211761fa9560b2c8794b2e698
tengyangdada <1469516907@qq.com>
2018-04-22 22:04:37 +0800

    commit

---
+++ First_Term/tengyang/Work_2/app/src/main/java/TitleLayout.java
@@ -0,0 +1,32 @@
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+
+public class TitleLayout extends LinearLayout {
+    public TitleLayout(Context context, AttributeSet attrs){
+        super(context,attrs);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+        Button titleBack=(Button)findViewById(R.id.button1);
+        Button titleEdit=(Button)findViewById(R.id.button2);
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity)getContext()).finish();
+            }
+        });
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
