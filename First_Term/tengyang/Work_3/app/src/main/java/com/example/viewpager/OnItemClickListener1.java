b2728e6b694ecce6dd7af92c6eebd25036bef889
tengyangdada <1469516907@qq.com>
2018-05-08 16:31:20 +0800

    commit

---
+++ First_Term/tengyang/Work_3/app/src/main/java/com/example/viewpager/OnItemClickListener1.java
@@ -0,0 +1,15 @@
+package com.example.viewpager;
+
+import android.view.View;
+
+
+public interface OnItemClickListener1 {
+    /**
+     * 回调接口
+     * @param view
+     * @param position
+     * @param str
+     */
+    void onItemClick(View view, int position, String str);
+}
+
