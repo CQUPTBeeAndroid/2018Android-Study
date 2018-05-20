ec0889eb2125d2143ad047ebfab0a854715862bf
ZMissL <1344627026@qq.com>
2018-05-20 11:01:27 +0800

    重新提交的作业

---
+++ Second_Term/liyunfeng/week_4/work_1/5/app/src/main/java/com/example/administrator/String.java
@@ -0,0 +1,14 @@
+package com.example.administrator;
+
+public class String {
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    private String data;
+
+}
